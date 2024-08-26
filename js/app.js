09cf12e66ed570aebabd0a89e663c595c4308290
Ajmal Hakim <samimajmal@yahoo.com>
2024-08-25 20:31:01 -0400

    add array creations types in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
 console.log("Sanity check");
+
+// Array an colletion of values(elment), 
+// Eech value called and elemtn
+// each element has a position(index) zero based
+// data type of element can be number,String, boolean, object, array or mix
+//array in js are dynmaic (they shrink and grow as need)
+// length ( number of element)
+
+let name ="Alex" ;
+
+let name2 ="Adam";
+
+let name3 = "Lily";
+
+// Declaation 
+
+
+// 1  using  Array() Constructor 
+let arr = new Array();
+console.log(arr);
+
+// 2
+
+let arr2 = [];
+
+
+// 3 
+
+let myArr = Array.of(1,2,3,5);
+
+console.table(myArr);
+
+
+
+
+
+let names = ["Aelx","Adam","Lily"]
+
+console.log(names);
+
+console.log("Size of the array "+names.length);
+
+console.table(names);
+
+
+
+
+
+
