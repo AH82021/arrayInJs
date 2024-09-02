ffaff645a49493d8609a19fa640fb12a3ccb6aa2
Ajmal Hakim <samimajmal@yahoo.com>
2024-09-01 20:45:22 -0400

    add create/update & remove element in the DOM

--- domLec/js/app.js
+++ domLec/js/app.js
@@ -32,3 +32,37 @@ let qs = document.querySelector('#main')
 
 let qsA = document.querySelectorAll("li")
 console.log(qsA);
+
+// 
+
+let demoEle = document.getElementById("demo");
+
+console.log(demoEle.innerText);
+console.log(demoEle.innerHTML);
+
+
+
+demoEle.innerHTML ="New Text <a>Hello <a/>"
+
+ let link = document.getElementById("myLink")
+
+link.textContent ="Amazon"
+link.setAttribute('href',"https://amazon.com")
+link.removeAttribute('href')
+
+let paragraph = document.getElementById("myPara")
+
+//
+paragraph.classList.add ='new_class'
+paragraph.classList.remove('highlight')
+
+console.log(paragraph.classList);
+
+// creating element
+let newElement = document.createElement('h3')
+newElement.textContent ="Eat healthy , Live longer"
+
+document.body.appendChild(newElement)
+
+newElement.remove()
+//Evnet handling
