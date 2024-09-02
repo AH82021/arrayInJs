d6ac58ca74f1b0d8e30e49ea8a8afeeeeb9b6025
Ajmal Hakim <samimajmal@yahoo.com>
2024-09-01 20:05:47 -0400

    selecting Elements js methods

--- domLec/js/app.js
+++ domLec/js/app.js
@@ -1,1 +1,34 @@
 console.log("DOM");
+
+// Document Object Model
+
+console.log(document);
+
+let ele = document.getElementById('main')
+console.log(ele);
+
+ele.textContent ="Hello, Dom"
+
+
+// 
+
+let eleByClass = document.getElementsByClassName('classA')
+
+console.log(eleByClass);
+
+ for(let i =0; i<eleByClass.length;i++){
+   if( i % 2 == 1){
+    eleByClass[i].textContent ="Faraz"
+   }
+ }
+
+
+ let h1Col = document.getElementsByTagName('h1')
+
+ console.log(h1Col.length)
+
+
+let qs = document.querySelector('#main')
+
+let qsA = document.querySelectorAll("li")
+console.log(qsA);
