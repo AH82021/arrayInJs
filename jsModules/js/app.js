cec6d82f6c2afed481955533e7a9a6049b2c09cc
Ajmal Hakim <samimajmal@yahoo.com>
2024-09-15 19:20:28 -0400

    add jsModules basic

---
+++ jsModules/js/app.js
@@ -0,0 +1,15 @@
+import {add} from './math.js'
+// Product add
+// Store   add
+// Managment  add
+
+// Code Organiziation 
+// Reusabilty 
+// Encapsulaiton // 
+//Maintainabilty 
+
+// modules 
+// ESM (EcamaScript Module )
+
+const result = add(2,3);
+console.log(result);
